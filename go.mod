7700fd3a9b051f2598bfffa560c76fd3a9b45416
Merlin Schumacher <merlin.schumacher@gmail.com>
2019-08-27 14:34:02 +0200

    initial data

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/merlinschumacher/csv-dns
+
+go 1.12
+
+require github.com/miekg/dns v1.1.16 // indirect
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+type DNSEntry struct {
+	shortname string
+	fullname  string
+}
+
+func main() {
+	loadCSV("dns.csv")
+}
+
+func loadCSV(filename string) {
+
+}
